e27ccad1793062180df0540957bb59f4d9de3a98
gildarts <gildarts@orbital.rocks>
2017-11-28 19:39:05 +0000

    add basic bantracker command file

---
+++ cmd/bantracker/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+
+	// Load plugins
+	// _ "github.com/belak/go-seabird-bucket"
+
+	// Load the core
+	"github.com/belak/go-seabird"
+)
+
+func failIfErr(err error, desc string) {
+	if err != nil {
+		logrus.WithError(err).Fatalln(desc)
+	}
+}
+
+func main() {
+	// Seed the random number generator for plugins to use.
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	conf := os.Getenv("SEABIRD_CONFIG")
+	if conf == "" {
+		conf = "config.toml"
+		_, err := os.Stat(conf)
+		failIfErr(err, "Failed to load config")
+	}
+
+	confReader, err := os.Open(conf)
+	failIfErr(err, "Failed to load config")
+
+	// Create the bot
+	b, err := seabird.NewBot(confReader)
+	failIfErr(err, "Failed to create new bot")
+
+	// Run the bot
+	err = b.ConnectAndRun()
+	failIfErr(err, "Failed to create run bot")
+}
